9e64d2bafe6b6c7bf2bb10ab6762ccaabf0aaaf3
shital chitalkar <shitalchitalkar9@gmail.com>
2026-01-14 23:14:12 +0530

    add my new signup and signinuser api commit

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,4 +1,139 @@
 const pool = require("../config/db");
+const bcrypt = require("bcryptjs");
+
+const jwt = require("jsonwebtoken");
+const JWT_SECRET = "mysecretkey";
+
+
+//signup user
+const signupUser =async (req,res)=>{
+    try{
+        const{fullname,email,password}=req.body;
+//validation
+        if(!fullname||!email||!password){
+            return res.status(401).json({
+                message:"user attribute are reaquired"
+            });
+        }
+
+        const existuser=await pool.query(
+      "SELECT id FROM users WHERE email = $1",
+      [email]);
+
+      if(existuser.rows.length>0){
+        return res.status(400).json({
+            success: false,
+            message:"user not fround"
+        });
+      }
+
+       // hash password
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    const result = await pool.query(
+      `INSERT INTO users (full_name, email, password, role)
+       VALUES ($1, $2, $3, 'user')
+       RETURNING id, full_name, email, role`,
+      [full_name, email, hashedPassword]
+    );
+
+      res.status(201).json({
+      success: true,
+      message: "Signup successful",
+      user: result.rows[0]
+    });
+
+  } catch (error) {
+    
+     res.status(500).json({
+      success: false,
+      message: "Server error"
+    });
+  }
+};
+
+//signinuser
+
+
+
+        
+ 
+
+const signinUser = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    // validation
+    if (!email || !password) {
+      return res.status(400).json({
+        success: false,
+        message: "Email and password required"
+      });
+    }
+
+    // check user exists
+    const result = await pool.query(
+      "SELECT * FROM users WHERE email = $1",
+      [email]
+    );
+
+    if (result.rows.length === 0) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password"
+      });
+    }
+
+    const user = result.rows[0];
+
+    // password compare
+    const isMatch = await bcrypt.compare(password, user.password);
+
+    if (!isMatch) {
+      return res.status(401).json({
+        success: false,
+        message: "Invalid email or password"
+      });
+    }
+
+    // create token
+    const token = jwt.sign(
+      {
+        id: user.id,
+        role: user.role
+      },
+      JWT_SECRET,
+      { expiresIn: "1d" }
+    );
+
+    return res.status(200).json({
+      success: true,
+      message: "Signin successful",
+      token,
+      user: {
+        id: user.id,
+        email: user.email,
+        role: user.role
+      }
+    });
+
+  } catch (error) {
+    return res.status(500).json({
+      success: false,
+      message: "Server error"
+    });
+  }
+};
+
+
+  
+
+
+
+
+
+
+
 
 
 //create user
@@ -59,7 +194,7 @@ const deleteUser =async (req,res)=>{
 }
 
 
-module.exports={createUser,getUsers,updateUser,deleteUser};
+module.exports={createUser,getUsers,updateUser,deleteUser,signupUser,signinUser};
 
 
 
